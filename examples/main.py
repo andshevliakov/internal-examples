c4e80a430cfe91e8a33c922cae806f8c0f33df37
andrii <andrii@ldap.example.com>
2021-06-30 14:24:32 +0300

    new

--- examples/main.py
+++ examples/main.py
@@ -4,32 +4,34 @@ from airflow import DAG
 from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 from airflow.models import Variable
 
-local_tz = pendulum.timezone("Europe/Kiev")
+local_tz = pendulum.timezone("Asia/Tehran")
 default_args = {
-    'owner': 'andrii',
+    'owner': 'mahdyne',
     'depends_on_past': False,
-    'start_date': datetime(2021, 6, 25, tzinfo=local_tz),
-    'email': ['shevlyakov.tlkpi@gmail.com'],
+    'start_date': datetime(2020, 10, 10, tzinfo=local_tz),
+    'email': ['nematpour.ma@gmail.com'],
     'email_on_failure': True,
     'email_on_retry': True,
     'retries': 2,
     'retry_delay': timedelta(minutes=5)
 }
 
-dag = DAG(dag_id='temp_dag',
+dag = DAG(dag_id='flight_search_dag',
           default_args=default_args,
           catchup=False,
           schedule_interval="0 * * * *")
 
-flight_search_ingestion= SparkSubmitOperator(task_id='search_ingestion',
+pyspark_app_home=Variable.get("PYSPARK_APP_HOME")
+
+flight_search_ingestion= SparkSubmitOperator(task_id='flight_search_ingestion',
 conn_id='spark_local',
-application='/temp.py',
+application=f'{pyspark_app_home}/temp.py',
 total_executor_cores=4,
 packages="io.delta:delta-core_2.12:0.7.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0",
 executor_cores=2,
 executor_memory='5g',
 driver_memory='5g',
-name='search_ingestion',
+name='flight_search_ingestion',
 execution_timeout=timedelta(minutes=10),
 dag=dag
-                                             )
+)
