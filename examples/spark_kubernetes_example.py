34faa181aef21a9723789ca68d6780323b7ef2ea
andriishevliakov <andrew.shevliakov@hpe.com>
2021-07-16 13:05:06 +0300

    changed job

--- examples/spark_kubernetes_example.py
+++ examples/spark_kubernetes_example.py
@@ -65,7 +65,7 @@ dag = DAG(
 
 submit = SparkKubernetesOperator(
     task_id='spark_pi_submit',
-    namespace="sampletenant",
+    namespace="airflow-tenant",
     application_file="example_spark_kubernetes_operator_pi.yaml",
     kubernetes_conn_id="kubernetes_in_cluster",
     do_xcom_push=True,
@@ -75,7 +75,7 @@ submit = SparkKubernetesOperator(
 
 sensor = SparkKubernetesSensor(
     task_id='spark_pi_monitor',
-    namespace="sampletenant",
+    namespace="airflow-tenant",
     application_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')['metadata']['name'] }}",
     kubernetes_conn_id="kubernetes_in_cluster",
     dag=dag,
