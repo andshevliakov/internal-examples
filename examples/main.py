4c1ecd90bb1a16c516e893cf3cb256f6c1f6cf56
andriishevliakov <andrew.shevliakov@hpe.com>
2021-07-16 19:22:30 +0300

    try

--- examples/main.py
+++ examples/main.py
@@ -1,34 +1,86 @@
-from datetime import datetime, timedelta
-import pendulum
-from airflow import DAG
-from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
-from airflow.models import Variable
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""
+This is an example DAG which uses SparkKubernetesOperator and SparkKubernetesSensor.
+In this example, we create two tasks which execute sequentially.
+The first task is to submit sparkApplication on Kubernetes cluster(the example uses spark-pi application).
+and the second task is to check the final state of the sparkApplication that submitted in the first state.
+Spark-on-k8s operator is required to be already installed on Kubernetes
+https://github.com/GoogleCloudPlatform/spark-on-k8s-operator
+"""
 
-local_tz = pendulum.timezone("Europe/Kiev")
+from datetime import timedelta, datetime
+
+# [START import_module]
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+# Operators; we need this to operate!
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+from airflow.utils.dates import days_ago
+
+# [END import_module]
+
+# [START default_args]
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
 default_args = {
     'owner': 'andrii',
     'depends_on_past': False,
-    'start_date': datetime(2020, 10, 10, tzinfo=local_tz),
-    'retries': 2,
-    'retry_delay': timedelta(minutes=5)
+    'start_date': days_ago(1),
+    'email': ['andrew.shevliakov@hpe.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'max_active_runs': 1,
+    'retries': 3
 }
+# [END default_args]
 
-dag = DAG(dag_id='flight_search_dag',
-          default_args=default_args,
-          catchup=False,
-          schedule_interval="0 * * * *")
+# [START instantiate_dag]
 
-pyspark_app_home=Variable.get("PYSPARK_APP_HOME")
-
-flight_search_ingestion=SparkSubmitOperator(task_id='flight_search_ingestion',
-conn_id='spark',
-application=f'{pyspark_app_home}temp.py',
-total_executor_cores=4,
-packages="io.delta:delta-core_2.12:0.7.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0",
-executor_cores=2,
-executor_memory='5g',
-driver_memory='5g',
-name='flight_search_ingestion',
-execution_timeout=timedelta(minutes=10),
-dag=dag
+dag = DAG(
+    'example_Hello',
+    default_args=default_args,
+    schedule_interval=timedelta(days=1),
+    tags=['example']
 )
+
+# spark = open(
+#     "example_spark_kubernetes_operator_pi.yaml").read()
+
+submit = SparkKubernetesOperator(
+    task_id='spark_pi_submit',
+    namespace="sampletenant",
+    application_file="example_spark_kubernetes_operator_pi.yaml",
+    kubernetes_conn_id="kubernetes_in_cluster",
+    do_xcom_push=True,
+    dag=dag,
+    api_group="sparkoperator.hpe.com"
+)
+
+sensor = SparkKubernetesSensor(
+    task_id='spark_pi_monitor',
+    namespace="sampletenant",
+    application_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')['metadata']['name'] }}",
+    kubernetes_conn_id="kubernetes_in_cluster",
+    dag=dag,
+    api_group="sparkoperator.hpe.com",
+    attach_log=True
+)
+
+submit >> sensor
